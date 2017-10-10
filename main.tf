9b1858dd469dfd5ce23dc6bb7fe1b19208d1feb5
Paul Williams <paul@skenmy.com>
2017-10-10 09:09:59 +0100

    Accept list of instances
    This helps when using the count parameter in resources.

--- main.tf
+++ main.tf
@@ -5,7 +5,8 @@ data "aws_region" "current" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "status_check_failed_instance_alarm_reboot" {
-  alarm_name          = "${var.instance_id}-StatusCheckFailedInstanceAlarmReboot"
+  count               = "${length(var.instances)}"
+  alarm_name          = "${element(var.instances, count.index)}-StatusCheckFailedInstanceAlarmReboot"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "5"
   metric_name         = "StatusCheckFailed_Instance"
@@ -17,14 +18,14 @@ resource "aws_cloudwatch_metric_alarm" "status_check_failed_instance_alarm_reboo
   alarm_description   = "Status checks have failed, rebooting system"
 
   dimensions {
-    InstanceId = "${var.instance_id}"
+    InstanceId = "${element(var.instances, count.index)}"
   }
 
   alarm_actions = ["arn:aws:swf:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:action/actions/AWS_EC2.InstanceId/Reboot/1.0"]
 }
 
 resource "aws_cloudwatch_metric_alarm" "status_check_failed_system_alarm_recover" {
-  alarm_name          = "${var.instance_id}-StatusCheckFailedSystemAlarmRecover"
+  alarm_name          = "${element(var.instances, count.index)}-StatusCheckFailedSystemAlarmRecover"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = "2"
   metric_name         = "StatusCheckFailed_System"
@@ -36,7 +37,7 @@ resource "aws_cloudwatch_metric_alarm" "status_check_failed_system_alarm_recover
   alarm_description   = "Status checks have failed for system, recovering instance"
 
   dimensions {
-    InstanceId = "${var.instance_id}"
+    InstanceId = "${element(var.instances, count.index)}"
   }
 
   alarm_actions = ["arn:aws:automate:${data.aws_region.current.name}:ec2:recover"]
